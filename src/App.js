d441f6d0467f18e312efe68d28f084c5d456d6a8
Ben Silveston <bsilveston37@gmail.com>
2021-07-13 22:05:45 +0100

    Defined and added the global CSS styling for the project.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
-  return (
-    <div className="App">
-      Start here.
-    </div>
-  );
+	return (
+		<div className="App">
+			Start here.
+			<GlobalStyle />
+		</div>
+	);
 }
 
 export default App;
