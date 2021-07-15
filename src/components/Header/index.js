d6e978c86626436d2208f74ec0130cbe47d59f95
Ben Silveston <bsilveston37@gmail.com>
2021-07-15 22:25:13 +0100

    Added the main content for the Header component.

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import RMDBLogo from "../../images/react-movie-logo.svg";
+import TMDBLogo from "../../images/tmdb_logo.svg";
+
+import { Wrapper, Content, LogoImage, TMDBLogoImage } from "./Headers.styles";
+
+const Header = () => (
+	<Wrapper>
+		<Content>
+			<LogoImage src={RMDBLogo} alt="rmdb-logo" />
+			<TMDBLogoImage src={TMDBLogo} alt="tmdb-logo" />
+		</Content>
+	</Wrapper>
+);
+
+export default Header;
