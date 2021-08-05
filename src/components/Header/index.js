9241a028b01a9591da4a8831182ead28abb16ddf
Ben Silveston <bsilveston37@gmail.com>
2021-08-05 21:11:10 +0100

    Renamed the header styles filename and updated associated import.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import RMDBLogo from "../../images/react-movie-logo.svg";
 import TMDBLogo from "../../images/tmdb_logo.svg";
 
-import { Wrapper, Content, LogoImage, TMDBLogoImage } from "./Headers.styles";
+import { Wrapper, Content, LogoImage, TMDBLogoImage } from "./Header.styles";
 
 const Header = () => (
 	<Wrapper>
