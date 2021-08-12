0df0c87e8712bed71d5a643165d108cad8fc933b
Ben Silveston <bsilveston37@gmail.com>
2021-08-12 12:46:18 +0100

    Transferred the previous code to a newly created custom hook.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,42 +1,23 @@
 import React, { useState, useEffect } from "react";
 import API from "../API";
-
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 import NoImage from "../images/no_image.jpg";
+import HeroImage from "./HeroImage";
+import { useHomeFetch } from "../hooks/useHomeFetch";
 
 const Home = () => {
-	const [state, setState] = useState();
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState(false);
-
-	const fetchMovies = async (page, searchTerm = "") => {
-		try {
-			setError(false);
-			setLoading(true);
-
-			const movies = await API.fetchMovies(searchTerm, page);
-
-			setState((prev) => ({
-				...movies,
-				results:
-					page > 1
-						? [...prev.results, ...movies.results]
-						: [...movies.results],
-			}));
-		} catch (error) {
-			setError(true);
-		}
-
-		setLoading(false);
-	};
-
-	useEffect(() => {
-		fetchMovies(1);
-	}, []);
-
-	console.log(state);
-
-	return <div>Home Page</div>;
+	const { state, loading, error } = useHomeFetch();
+	return (
+		<React.Fragment>
+			{state.results[0] ? (
+				<HeroImage
+					image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+					title={state.results[0].original_title}
+					text={state.results[0].overview}
+				/>
+			) : null}
+		</React.Fragment>
+	);
 };
 
 export default Home;
