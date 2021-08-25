19642ab92018f1b6deceb61067018949b0c051c7
Ben Silveston <bsilveston37@gmail.com>
2021-08-25 22:12:15 +0100

    Added the 'Grid' component to the application.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import API from "../API";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 import NoImage from "../images/no_image.jpg";
 import HeroImage from "./HeroImage";
+import Grid from "./Grid";
 import { useHomeFetch } from "../hooks/useHomeFetch";
 
 const Home = () => {
@@ -16,6 +17,11 @@ const Home = () => {
 					text={state.results[0].overview}
 				/>
 			) : null}
+			<Grid header="Popular Movies">
+				{state.results.map((movie) => (
+					<div key={movie.id}>{movie.title}</div>
+				))}
+			</Grid>
 		</React.Fragment>
 	);
 };
