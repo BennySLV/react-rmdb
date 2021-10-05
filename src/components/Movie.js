aaf50f5af3b0e91fafa1755962008be981df38f0
Ben Silveston <bsilveston37@gmail.com>
2021-10-05 22:05:32 +0100

    Connected the 'Movie' component with the 'useMovieFetch' hook.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+import Grid from "./Grid";
+import LoadingSpinner from "./LoadingSpinner";
+import NoImage from "../images/no_image.jpg";
+import { useMovieFetch } from "../hooks/useMovieFetch";
 
-const Movie = () => <div>Movie</div>;
+const Movie = () => {
+	const { movieId } = useParams();
+	const { state: movie, loading, error } = useMovieFetch(movieId);
+	console.log(movie);
+	return (
+		<React.Fragment>
+			<div>Movie</div>
+		</React.Fragment>
+	);
+};
 
 export default Movie;
