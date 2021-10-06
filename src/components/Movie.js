46301906c4beee7929580c9857a51c37e6544cd8
Ben Silveston <bsilveston37@gmail.com>
2021-10-06 21:55:35 +0100

    Added the 'MovieInfoBar' component to the 'Movie' component.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,6 +5,7 @@ import BreadCrumb from "./BreadCrumb";
 import Grid from "./Grid";
 import LoadingSpinner from "./LoadingSpinner";
 import MovieInfo from "./MovieInfo";
+import MovieInfoBar from "./MovieInfoBar";
 import NoImage from "../images/no_image.jpg";
 import { useMovieFetch } from "../hooks/useMovieFetch";
 
@@ -17,6 +18,11 @@ const Movie = () => {
 		<React.Fragment>
 			<BreadCrumb movieTitle={movie.original_title} />
 			<MovieInfo movie={movie} />
+			<MovieInfoBar
+				time={movie.runtime}
+				budget={movie.budget}
+				revenue={movie.revenue}
+			/>
 		</React.Fragment>
 	);
 };
